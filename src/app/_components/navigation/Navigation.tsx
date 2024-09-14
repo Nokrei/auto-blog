d413fff50f43ee336dd43b5eaf233c318830ec8f
piotr <dive877@gmail.com>
2024-09-14 14:36:06 +0400

    add navigation and initial page

---
+++ src/app/_components/navigation/Navigation.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Free",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A free plan that allows you try out the platform with limited features.",
+  },
+  {
+    title: "Pro",
+    href: "/docs/primitives/hover-card",
+    description: "Unlock all the features of the platform with the Pro plan.",
+  },
+  {
+    title: "Enteprise",
+    href: "/docs/primitives/progress",
+    description:
+      "Folly custom solutions for your business with the Enterprise plan.",
+  },
+];
+
+export function Navigation() {
+  return (
+    <NavigationMenu className="mx-auto bg-zinc-900 rounded-xl my-5 bg-primary text-secondary">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <p className="text-7xl">AB</p>
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Auto Blog
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      A platform that lets you generate blogs with AI.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                How does this work?
+              </ListItem>
+              <ListItem href="/docs/installation" title="Pricing">
+                Our pricing plans
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Use Cases">
+                What can I make with this?
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Pricing</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main className="">Hello world</main>;
+  return (
+    <main className="max-w-7xl mx-auto">
+      <section className="min-h-[calc(100vh-200px)] flex flex-col items-center justify-center gap-10 md:gap-5">
+        <h1 className="text-9xl font-bold text-center">Auto Blog</h1>
+        <div className="flex gap-5">
+          <p>Create</p>
+          <p>Generate</p>
+          <p>Sit back</p>
+        </div>
+      </section>
+    </main>
+  );
 }
